090c7edf1cdf71d6570a0483b9b49ba0c84175c9
Andrii <andrewkrinskiy@gmail.com>
2020-03-06 16:12:33 +0200

    switch to pug template

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,14 @@
+const signInButton = document.getElementById("signInBtn");
+const overlay = document.querySelector(".overlay");
 
+signInButton.addEventListener("click", (e) => {
+    overlay.classList.add("overlay-visible")
+});
+
+
+overlay.addEventListener("click", e => {
+    const item = e.target;
+    if(item.classList.contains("overlay")) {
+        overlay.classList.remove("overlay-visible")
+    }
+});
