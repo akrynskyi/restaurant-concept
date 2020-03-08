3c2f67236b6fc198803dafa7e69130d9dc335dda
Andrii Krynskyi <Andrii_Krynskyi@epam.com>
2020-03-08 17:40:09 +0200

    add search-bar feature

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,20 @@
 const signInButton = document.getElementById("signInBtn");
 const overlay = document.querySelector(".overlay");
 
-signInButton.addEventListener("click", (e) => {
-    overlay.classList.add("overlay-visible")
+signInButton.addEventListener("click", () => {
+  overlay.classList.add("overlay-visible");
 });
 
-
 overlay.addEventListener("click", e => {
-    const item = e.target;
-    if(item.classList.contains("overlay")) {
-        overlay.classList.remove("overlay-visible")
-    }
+  const item = e.target;
+  if (item.classList.contains("overlay")) {
+    overlay.classList.remove("overlay-visible");
+  }
+});
+
+const searchButton = document.getElementById("searchBtn");
+const searchBar = document.querySelector(".search-bar");
+
+searchButton.addEventListener("click", () => {
+  searchBar.classList.toggle("search-bar-active");
 });
