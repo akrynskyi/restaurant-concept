4dd1af20a0d085898b5e9f0f55cab3cf7b5ce30b
Andrii <andrewkrinskiy@gmail.com>
2020-03-10 17:27:17 +0200

    add spinner

--- src/js/firebase/authentication.js
+++ src/js/firebase/authentication.js
@@ -6,16 +6,23 @@ const signInForm = document.getElementById("signInForm");
 const emailDOM = signInForm["signInEmail"];
 const passwordDOM = signInForm["signInPassword"];
 
+const spinner = document.getElementById("loader");
+
 signInForm.addEventListener("submit", e => {
   e.preventDefault();
   const email = emailDOM.value;
   const password = passwordDOM.value;
+  spinner.classList.add("loader-container-active");
 
   // sign up test
-  auth.createUserWithEmailAndPassword(email, password).then(data => {
-    console.log(data);
-    ui.hideModalDefault();
-    signInForm.reset();
+  auth.createUserWithEmailAndPassword(email, password)
+      .then(data => {
+          console.log(data);
+          spinner.classList.remove("loader-container-active");
+          ui.hideModalDefault();
+          signInForm.reset();
+  }).catch(error => {
+    console.error(error)
   });
 });
 
